a3346877dba25622086d459485ae31fc8638c8e8
володя вовчко <2vova1@ukr.net>
2022-12-28 00:28:12 +0200

    Mapper interface created

---
+++ src/main/java/com/itstep/exam/mapper/Mapper.java
@@ -0,0 +1,24 @@
+package com.itstep.exam.mapper;
+
+import com.itstep.exam.dto.DeliveryDto;
+import com.itstep.exam.dto.TransporterDto;
+import com.itstep.exam.dto.WarehouseDto;
+import com.itstep.exam.entity.Delivery;
+import com.itstep.exam.entity.Transporter;
+import com.itstep.exam.entity.Warehouse;
+import org.mapstruct.MappingTarget;
+
+@org.mapstruct.Mapper(componentModel = "spring")
+public interface Mapper {
+    WarehouseDto toDto(Warehouse warehouse);
+    Warehouse toEntity(WarehouseDto dto);
+    void merge(WarehouseDto warehouseDto, @MappingTarget Warehouse warehouse);
+
+    DeliveryDto toDto(Delivery delivery);
+    Delivery toEntity(DeliveryDto dto);
+    void merge(DeliveryDto dto, @MappingTarget Delivery delivery);
+
+    TransporterDto toDto(Transporter transporter);
+    Transporter toEntity(TransporterDto dto);
+    void merge(TransporterDto dto, @MappingTarget Transporter transporter);
+}
